e1c756d245eb3fe0d4f3544c36f424bb8c141fe3
Yan Harazhun <harazhun06@gmail.com>
2025-06-06 20:58:09 +0200

    added new components to split the App

---
+++ components/task-input/Task-input-styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const taskInputStyles = StyleSheet.create({
+    input: {
+    width: 200,
+    height: 50,
+    padding: 10,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: "#57C785",
+  },
+});
